2a141c814cd2f5a42c0dea579d750bb33ee1bc84
Arijitbera213 <56600106+Arijitbera213@users.noreply.github.com>
2021-08-07 12:42:38 +0530

    main update firebase connected

--- data.js
+++ data.js
@@ -1,20 +1,47 @@
-window.addEventListener('load', () => {
+// Initialize Firebase
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyBR5ADeIWMZ0YUSPhvCMT_IISCa-cmb4EE",
+  authDomain: "stock-data-fc41e.firebaseapp.com",
+  projectId: "stock-data-fc41e",
+  storageBucket: "stock-data-fc41e.appspot.com",
+  messagingSenderId: "31054906120",
+  appId: "1:31054906120:web:6a511eb07276159eb0a2bd"
+};
+firebase.initializeApp(firebaseConfig);
 
-    
-    const sd = sessionStorage.getItem('sd');
-    const ed = sessionStorage.getItem('ed');
-    
-    document.getElementById('sd').innerHTML = sd;
-    document.getElementById('ed').innerHTML = ed;
-    var fs=require('fs'); 
- 
-
-    fs.writeFile('data.txt', sd, function(err, result) {
-        if(err) console.log('error', err);
-      });
-      fs.writeFile('data.txt', ed, function(err, result) {
-        if(err) console.log('error', err);
-      });
+//Reference messages collection
+  // Reference Messages collection
+  let contactInfo = firebase.database().ref("information");
 
 
-})
+
+
+  document.querySelector(".contact-form").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+    e.preventDefault();
+  
+
+    let sd= document.querySelector(".sd").value;
+    let ed = document.querySelector(".ed").value;
+  
+    console.log( sd,ed);
+  
+    saveContactInfo(sd,ed);
+    alert("Your Message has been sent Successfully. Thank You!");
+    document.querySelector(".contact-form").reset();
+  }
+  
+  
+  
+  
+  function saveContactInfo(sd,ed) {
+    let newContactInfo = contactInfo.push();
+  
+    newContactInfo.set({
+      sd: sd,
+      ed: ed,
+      
+    });
+  }
